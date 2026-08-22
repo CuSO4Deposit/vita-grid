bd8dfc76501f3737846578be669082d5a269a29f
cocvu <cocvu@nightcord.unknown.tld>
2026-08-22 14:47:56 +0000

    introduce config file

--- aggregator/main.go
+++ aggregator/main.go
@@ -2,19 +2,49 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Listen  string            `json:"listen"`
+	Sources []json.RawMessage `json:"sources"`
+}
+
 type Signal struct {
-	Name 	string 	`json:"name"`
-	State 	string 	`json:"state"`
-	Busy 	bool 	`json:"busy"`
+	Name  string `json:"name"`
+	State string `json:"state"`
+	Busy  bool   `json:"busy"`
 }
 
 func main() {
+	configPath := flag.String("config", "config.json", "config file path")
+	flag.Parse()
+
+	config, err := loadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("Load config failed: %v", err)
+	}
+
 	http.HandleFunc("/status", handleStatus)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(config.Listen, nil))
+}
+
+func loadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var c Config
+	err = json.Unmarshal(data, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
 }
 
 func handleStatus(w http.ResponseWriter, _ *http.Request) {
